c16cb8d169af53da04e66acc2330ee548589e7db
Mari <38698671+Maridmi@users.noreply.github.com>
2020-07-13 23:03:48 +0300

    Add files via upload

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a, .row a, .panel a").on('click', function(event) {
+  $(".totop a").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -16,6 +16,34 @@ $(document).ready(function(){
       }, 900, function(){
    
         // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+
+$(document).ready(function(){
+  $(".tofirst a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+      
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
         window.location.hash = hash;
       });
     }
